3a6f9bcbc309d70f9bfdb33d97b18945d10aab9f
Haflidi Fridthjofsson <26624010+haflidif@users.noreply.github.com>
2022-01-22 23:45:09 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,45 @@
+variable "resource_group_name" {
+  type = string
+  description = ""
+}
 
+variable "location" {
+  type = string
+  description = ""
+}
+
+variable "vnet_name" {
+  type = string
+  description = ""
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+  description = ""
+}
+
+variable "dns_servers" {
+  type = list(string)
+  description = ""
+  default = []
+}
+
+variable "ddos_plan" {
+  type = bool
+  description = ""
+  default = false
+}
+
+variable "subnets" {
+  default = {}
+}
+
+variable "delegation" {
+  description = "Subnet Delegation configuration"
+  type = list(object({
+    name         = string
+    service_name = string
+    actions      = list(string)
+  }))
+  default = []
+}
